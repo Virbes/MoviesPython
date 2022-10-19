32b9d6d1a669f8af8c3664d38ce142b1c689e388
Virbes <francisco.virbes.457@gmail.com>
2022-10-18 20:09:57 -0500

    Creando BD SQLite

--- ec.py
+++
@@ -1,8 +0,0 @@
-from database import get_db
-
-db, c = get_db()
-
-c.execute('SELECT sale_date FROM Sales WHERE id_sale = %s AND id_user = %s', (48724, 1))
-date_sale = c.fetchone()
-
-print(date_sale)
