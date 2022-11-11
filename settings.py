6ae23ab737367998ec53817dbe48f2a27ffaeded
Virbes <francisco.virbes.457@gmail.com>
2022-11-11 03:20:28 -0600

    100% Django x2

--- settings.py
+++
@@ -1,8 +0,0 @@
-DEBUG = True
-PORT = 8080
-SECRET_KEY = 'Secret'
-WTF_CSRF_ENABLED = True
-UPLOAD_FOLDER = 'static/images/upload/movies'
-UPLOAD_FOLDER_PROFILE = 'static/images/upload/profile_picture'
-
-ADMIN_USERS = ['Admin']
